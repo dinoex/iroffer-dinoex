7303c49943513be1b73c0a06df7e286b357f4a0a
dm <dm>
2005-10-18 13:39:55 +0000

    - detect complete strings

--- update-en.sh
+++ update-en.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "New in en.txt:"
 nr="0"
 if test -f en.txt
 then
@@ -6,12 +7,30 @@ then
 fi
 fgrep -h \" src/iroffer*.c |
 grep -v "^#include" |
-awk -F \" '
+awk -F \[\"\] '
 {
-	for ( I = 2; I <= NF ; I += 2 ) {
-		if ( $I == "" )
-			continue
-		print "\"" $I "\""
+	MORE = 0
+	for ( I = 2; I <= NF ; I ++ ) {
+		gsub( "[\\\\]", "\\\\", $I )
+		LEN = length( $I ) - 1
+		if ( substr( $I, LEN, 2 ) == "\\\\" ) {
+			printf( "\"%s\\", $I )
+			MORE = 1
+		} else {
+			if ( MORE == 0 ) {
+				if ( $I != "" )
+					print "\"" $I "\""
+				I ++
+			} else {
+				MORE = 0
+				if ( $I != "" ) {
+					print "\"" $I "\""
+				} else {
+					print $I "\""
+				}
+				I ++
+			}
+		}
 	}
 #	print
 #	print ""
@@ -27,5 +46,18 @@ do
 	echo "${nr} ${text}"
 	echo "${nr} ${text}" >> en.txt
 done
-
+#
+# 
+echo "Obsolete in en.txt:"
+sed -e 's|\\|\\\\|g' -e "s| \"| '\"|" -e "s|\"$|\"'|" en.txt |
+while read nr text
+do
+	text="${text#'}"
+	text="${text%'}"
+	if fgrep -q "${text}" src/*.c
+	then
+		continue
+	fi
+	echo "${nr} ${text}"
+done
 # eof
