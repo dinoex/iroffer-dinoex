e631823c9de68d7601ffd7ddffdab76eb9d5a8e4
dm <dm>
2008-04-19 14:00:59 +0000

    - make base64 private

--- src/dinoex_utilities.h
+++ src/dinoex_utilities.h
@@ -33,6 +33,5 @@ int get_port(ir_sockaddr_union_t *listenaddr);
 int strcmp_null(const char *s1, const char *s2);
 int is_file_writeable(const char *f);
 char *hostmask_to_fnmatch(const char *str);
-extern unsigned char base64decode[ 256 ];
 
 /* End of File */
