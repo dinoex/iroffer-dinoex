9a489cb35cc8e7b0837b5c56b69553f870cffb8b
cvs <cvs>
2009-01-11 20:09:49 +0000

    - ruby_script with auto_reload

--- src/dinoex_ruby.h
+++ src/dinoex_ruby.h
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2007-2008 Dirk Meyer
+ * Copyright (C) 2007-2009 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -16,7 +16,7 @@
 #ifdef USE_RUBY
 void do_myruby(char *line);
 void startup_myruby(void);
-void rehash_myruby(void);
+void rehash_myruby(int check);
 void shutdown_myruby(void);
 #endif /* USE_RUBY */
 
