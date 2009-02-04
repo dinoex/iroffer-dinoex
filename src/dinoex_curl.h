409af4ec2fec7b001fe000c86f2aa69997b0b655
cvs <cvs>
2009-02-04 19:40:38 +0000

    - bugfix: FETCHCANCEL

--- src/dinoex_curl.h
+++ src/dinoex_curl.h
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2004-2008 Dirk Meyer
+ * Copyright (C) 2004-2009 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -18,7 +18,7 @@ void curl_startup(void);
 void curl_shutdown(void);
 extern int fetch_started;
 void fetch_multi_fdset(fd_set *read_fd_set, fd_set *write_fd_set, fd_set *exc_fd_set, int *max_fd);
-void fetch_cancel(int num);
+int fetch_cancel(int num);
 void fetch_perform(void);
 void start_fetch_url(const userinput *const u);
 void dinoex_dcl(const userinput *const u);
