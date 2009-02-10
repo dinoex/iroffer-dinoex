385705ec9381bc06da3ffa3e361d54c9001c9353
cvs <cvs>
2009-02-10 21:59:55 +0000

    - reorder functions

--- src/dinoex_irc.h
+++ src/dinoex_irc.h
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2004-2008 Dirk Meyer
+ * Copyright (C) 2004-2009 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -13,6 +13,7 @@
  *
  */
 
+int my_getnameinfo(char *buffer, size_t len, const struct sockaddr *sa, socklen_t salen);
 void update_natip (const char *var);
 void update_server_welcome(char *line);
 int bind_irc_vhost(int family, int clientsocket);
@@ -20,8 +21,6 @@ int open_listen(int family, ir_sockaddr_union_t *listenaddr, int *listen_socket,
 int irc_open_listen(ir_connection_t *con);
 char *setup_dcc_local(ir_sockaddr_union_t *listenaddr);
 void child_resolver(void);
-int my_getnameinfo(char *buffer, size_t len, const struct sockaddr *sa, socklen_t salen);
-int my_dcc_ip_port(char *buffer, size_t len, ir_sockaddr_union_t *sa, socklen_t salen);
 const char *my_dcc_ip_show(char *buffer, size_t len, ir_sockaddr_union_t *sa, int net);
 int connectirc2(res_addrinfo_t *remote);
 
