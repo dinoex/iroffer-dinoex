cf341363a05f8b282e05b6350d35e067896920c6
cvs <cvs>
2009-02-20 17:32:09 +0000

    - redesign privmsgparse

---
+++ src/dinoex_user.h
@@ -0,0 +1,18 @@
+/*
+ * by Dirk Meyer (dinoex)
+ * Copyright (C) 2004-2009 Dirk Meyer
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the GNU General Public License.  More information is
+ * available in the LICENSE file.
+ *
+ * If you received this file without documentation, it can be
+ * downloaded from http://iroffer.dinoex.net/
+ *
+ * $Id$
+ *
+ */
+
+void privmsgparse(int type, int decoded, char* line);
+
+/* End of File */
