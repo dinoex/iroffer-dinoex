00fc742be56f5f9f248a84c71d3cbf748df90755
cvs <cvs>
2009-04-01 12:33:51 +0000

    - test

--- cygwin-gen-dist.sh
+++ cygwin-gen-dist.sh
@@ -1,28 +1,53 @@
 #!/bin/sh
-ver=`grep ^VERSION= Configure  | cut -d '=' -f2`
 #
 all="LICENSE README-iroffer.txt THANKS header.html footer.html"
 fen="README.modDinoex sample.config help-admin-en.txt"
 fde="LIESMICH.modDinoex beispiel.config help-admin-de.txt"
+fit="README.modDinoex sample.config help-admin-it.txt"
 #
 set -e
+ver=`grep ^VERSION= Configure  | cut -d '=' -f2`
 name="iroffer-dinoex-${ver}"
-./Configure -t -g -u
-./Lang de
-make
-mv iroffer.exe iroffer-de.exe 
-./Lang en
-make clean
-make
-mkdir "${name}-win32"
-mkdir "${name}-win32-de"
-rsync -av htdocs "${name}-win32/"
-rsync -av htdocs "${name}-win32-de/"
-zip -l a.zip ${all} ${fen} ${fde}
+#
+# Convert into DOS files
+zip -l a.zip ${all} ${fen} ${fde} ${fit}
 unzip -o a.zip
 rm -f a.zip
-cp -p ${all} ${fen} iroffer.exe "${name}-win32/"
-cp -p ${all} ${fde} iroffer-de.exe "${name}-win32-de/"
+#
+./Configure -t -g -u
+# Build translatetd versions
+for lang in de it en
+do
+	./Lang "${lang}"
+	make clean
+	make
+	case "${lang}" in
+	en)
+		dir="${name}-win32"
+		bin="iroffer.exe"
+		;;
+	*)
+		dir="${name}-win32-${lang}"
+		bin="iroffer-${lang}.exe"
+		;;
+	esac
+	mkdir "${dir}"
+	mv iroffer.exe "${dir}/${bin}"
+	rsync -av htdocs ${all} "${dir}/"
+	case "${lang}" in
+	en)
+		cp -p ${fen} "${dir}/"
+		;;
+	de)
+		cp -p ${fde} "${dir}/"
+		;;
+	it)
+		cp -p ${fit} "${dir}/"
+		;;
+	esac
+done
+# Build enlglish version
 zip -r "${name}-win32.zip" "${name}-win32/"
 zip -r "${name}-win32-de.zip" "${name}-win32-de/"
+zip -r "${name}-win32-it.zip" "${name}-win32-it/"
 # eof
