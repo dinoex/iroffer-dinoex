105158872086f7791ac466ffa9e874a092eb2fda
cvs <cvs>
2009-11-07 04:41:09 +0000

    - a_raw

--- src/dinoex_admin.h
+++ src/dinoex_admin.h
@@ -106,6 +106,7 @@ void a_getl(const userinput * const u);
 void a_get(const userinput * const u);
 void a_rmq(const userinput * const u);
 void a_rmiq(const userinput * const u);
+void a_raw(const userinput * const u);
 void a_rawnet(const userinput * const u);
 void a_lag(const userinput * const u);
 void a_bannnick(const userinput * const u);
