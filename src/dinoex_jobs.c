4e6d234ca8045fa0c3bdf3bc3f94f9db28d72d98
cvs <cvs>
2010-03-07 10:55:22 +0000

    - spaces

--- src/dinoex_jobs.c
+++ src/dinoex_jobs.c
@@ -1,13 +1,13 @@
-	/*
-	 * by Dirk Meyer (dinoex)
-	 * Copyright (C) 2004-2010 Dirk Meyer
-	 *
-	 * By using this file, you agree to the terms and conditions set
-	 * forth in the GNU General Public License.  More information is
-	 * available in the LICENSE file.
-	 *
-	 * If you received this file without documentation, it can be
-	 * downloaded from http://iroffer.dinoex.net/
+/*
+ * by Dirk Meyer (dinoex)
+ * Copyright (C) 2004-2010 Dirk Meyer
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the GNU General Public License.  More information is
+ * available in the LICENSE file.
+ *
+ * If you received this file without documentation, it can be
+ * downloaded from http://iroffer.dinoex.net/
  *
  * $Id$
  *
