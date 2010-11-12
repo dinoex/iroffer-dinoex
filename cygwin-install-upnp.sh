aa65b8d5f3da67bdbd692b3684dbfade3f0a1055
cvs <cvs>
2010-11-12 06:07:24 +0000

    - bugfix

--- cygwin-install-upnp.sh
+++ cygwin-install-upnp.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+ver="${1-1.4.20100609}"
+# 1.4 has regressions
 ver="${1-1.3}"
-name="miniupnpc-${1-1.3}"
+name="miniupnpc-${ver}"
 curl -o "${name}.tar.gz" "http://miniupnp.free.fr/files/${name}.tar.gz"
-tra -xvzf "${name}.tar.gz"
+tar -xvzf "${name}.tar.gz"
 cd "${name}" || exit 1
 make || exit 1
 make install
