683f1e71ac674b1f85890dcf44c99ade7e89ea14
cvs <cvs>
2011-02-20 16:36:40 +0000

    - bump miniupnc 1.5

--- cygwin-install-upnp.sh
+++ cygwin-install-upnp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-ver="${1-1.4.20100609}"
-# 1.4 has regressions
-ver="${1-1.3}"
+ver="${1-1.5}"
 name="miniupnpc-${ver}"
 curl -o "${name}.tar.gz" "http://miniupnp.free.fr/files/${name}.tar.gz"
 tar -xvzf "${name}.tar.gz"
