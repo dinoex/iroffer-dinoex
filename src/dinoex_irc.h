707624f0bebb3823287d048cab6bb93e9637460a
cvs <cvs>
2018-07-30 15:06:35 +0000

    - usenatip with IPv6

--- src/dinoex_irc.h
+++ src/dinoex_irc.h
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2004-2012 Dirk Meyer
+ * Copyright (C) 2004-2018 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -14,7 +14,7 @@
  */
 
 size_t my_getnameinfo(char *buffer, size_t len, const struct sockaddr *sa);
-void update_natip (const char *var);
+void update_natip(const char *var);
 unsigned int bind_irc_vhost(int family, int clientsocket);
 unsigned int open_listen(int family, ir_sockaddr_union_t *listenaddr, int *listen_socket, unsigned int port, unsigned int reuse, unsigned int search, const char *vhost);
 unsigned int irc_open_listen(ir_connection_t *con);
@@ -22,7 +22,7 @@ void ir_setsockopt(int clientsocket);
 char *setup_dcc_local(ir_sockaddr_union_t *listenaddr);
 void child_resolver(int family);
 void irc_resolved(void);
-const char *my_dcc_ip_show(char *buffer, size_t len, ir_sockaddr_union_t *sa, unsigned int net);
+int my_dcc_ip_show(char *buffer, size_t len, unsigned int net);
 
 char *get_local_vhost(void);
 char *get_config_nick(void);
