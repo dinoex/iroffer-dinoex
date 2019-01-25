a32c10ab3daf17ee3d707f29ebbe3fa98afe28a1
cvs <cvs>
2019-01-25 21:48:18 +0000

    - force versions strings

--- cygwin-install-upnp.sh
+++ cygwin-install-upnp.sh
@@ -4,6 +4,7 @@ name="miniupnpc-${ver}"
 curl -o "${name}.tar.gz" "http://miniupnp.free.fr/files/${name}.tar.gz"
 tar -xvzf "${name}.tar.gz"
 cd "${name}" || exit 1
+sh updateminiupnpcstrings.sh || exit 1
 make || exit 1
 make install
 # eof
