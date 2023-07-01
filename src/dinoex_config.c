dfc010afdfff8cfb5ad03fd915888ed79d06ec74
cvs <cvs>
2023-07-01 15:03:38 +0000

    - tag Copyright

--- src/dinoex_config.c
+++ src/dinoex_config.c
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2004-2022 Dirk Meyer
+ * Copyright (C) 2004-2023 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -9,7 +9,7 @@
  * If you received this file without documentation, it can be
  * downloaded from https://iroffer.net/
  *
- * SPDX-FileCopyrightText: 2004-2021 Dirk Meyer
+ * SPDX-FileCopyrightText: 2004-2023 Dirk Meyer
  * SPDX-License-Identifier: GPL-2.0-only
  *
  * $Id$
--- src/iroffer_utilities.c
+++ src/iroffer_utilities.c
@@ -10,7 +10,7 @@
  * downloaded from http://iroffer.org/
  *
  * SPDX-FileCopyrightText: 1998-2005 David Johnson
- * SPDX-FileCopyrightText: 2004-2021 Dirk Meyer
+ * SPDX-FileCopyrightText: 2004-2023 Dirk Meyer
  * SPDX-License-Identifier: GPL-2.0-only
  *
  * @(#) iroffer_utilities.c 1.205@(#)
