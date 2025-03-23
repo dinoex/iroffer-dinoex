28508106ce71803d7d1c9013dc3f7560c3c2f13f
cvs <cvs>
2025-03-23 15:30:50 +0000

    - disable -Wpadded for maxminddb

--- src/dinoex_maxminddb.c
+++ src/dinoex_maxminddb.c
@@ -1,6 +1,6 @@
 /*
  * by Dirk Meyer (dinoex)
- * Copyright (C) 2019-2021 Dirk Meyer
+ * Copyright (C) 2019-2025 Dirk Meyer
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the GNU General Public License.  More information is
@@ -9,7 +9,7 @@
  * If you received this file without documentation, it can be
  * downloaded from https://iroffer.net/
  *
- * SPDX-FileCopyrightText: 2019-2021 Dirk Meyer
+ * SPDX-FileCopyrightText: 2019-2025 Dirk Meyer
  * SPDX-License-Identifier: GPL-2.0-only
  *
  * $Id$
@@ -28,7 +28,10 @@
 
 #ifdef USE_MAXMINDDB
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpadded"
 #include <maxminddb.h>
+#pragma GCC diagnostic pop
 
 typedef struct {
   const char *gi;
